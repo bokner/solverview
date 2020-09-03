c50afd482d5f77dfbd4913f5617e9f8c2150d324
Boris Okner <boris.okner@gmail.com>
2020-09-03 17:28:07 -0400

    Switch to async solving, implement flow (i.e. new_puzzle -> solve -> show results -> ...)

--- lib/solverlview_web/live/sudoku.ex
+++ lib/solverlview_web/live/sudoku.ex
@@ -2,62 +2,76 @@ defmodule SolverlviewWeb.Sudoku do
   require Logger
   use Phoenix.LiveView
 
+  ## Stages
+  @start_new 1
+  @solving   2
+  @solved    3
+  @not_solved 4
+
   def mount(_params, _session, socket) do
     {
       :ok,
-      assign(
-        socket,
-        [
-          total_solutions: 0,
-          sudoku: empty_sudoku(),
-          solving: false
-        ]
-      )
+      new_puzzle(socket)
     }
   end
 
-  def handle_event(:new_solution, _, socket) do
-    {:noreply, update(socket, :total_solutions, &(&1 + 1))}
+  def handle_info({:solver_event, event, data}, socket) do
+    #Process.sleep(1000)
+    {:noreply, process_solver_event(event, data, socket)}
+  end
+
+  def handle_event("ignore", _, socket) do
+    {:noreply, socket}
+  end
+
+  def handle_event("next_puzzle", _, socket) do
+    {:noreply, new_puzzle(socket)}
   end
 
   def handle_event("solve", data, socket) do
     Logger.debug "Data: #{inspect data}, Socket: #{inspect socket}"
-    solve(data, socket)
-    {:noreply, solve(data, socket)}
+    solve(data)
+    {:noreply, socket}
   end
 
 
   def render(assigns) do
     ~L"""
-    <form phx-submit="solve" method="post" width="50%">
+    <div>
+    <h1># of solutions: <%= @total_solutions %></h1>
+    <form phx-submit="<%= action(@stage) %>" method="post">
       <div class="sudoku">
       <%= for i <- 0..8 do %>
         <div>
+          <tr>
           <%= for j <- 0..8 do %>
             <input maxlength="1" size="1" name="input[<%= i %>][<%= j %>]" value="<%= Enum.at(Enum.at(@sudoku, i), j) %>"
-              <%= if @solving, do: "disabled" %>/>
+              />
           <% end %>
+          </tr>
         </div>
       <% end %>
       </div>
-      <button>Start</button>
-
+      <button <%= if @stage == 2, do: "disabled" %>><%= button_name(@stage) %></button>
     </form>
+    </div>
     """
   end
 
 
-  defp solve(data, socket) do
-    puzzle = input_to_puzzle(data)
-    res = File.cd!(
+  defp solve(input) do
+    puzzle = input_to_puzzle(input)
+    my_pid = self()
+    {:ok, _pid} = File.cd!(
       Application.app_dir(:solverl, "priv"),
-      fn -> Sudoku.solve_sync(puzzle) end
+      fn ->
+        Sudoku.solve(
+          puzzle,
+          solution_handler: fn (event, data) -> send(my_pid, {:solver_event, event, data}) end
+        )
+      end
     )
-    solved_puzzle = MinizincResults.get_solution_value(
-      MinizincResults.get_last_solution(res),
-      "puzzle"
-    )
-    update(socket, :sudoku, fn _ -> solved_puzzle end)
+
   end
 
   defp input_to_puzzle(data) do
@@ -77,8 +91,73 @@ defmodule SolverlviewWeb.Sudoku do
   defp empty_sudoku() do
     Enum.map(
       0..8,
-      fn (r) -> Enum.map(0..8, fn _c -> "" end)
+      fn (_r) -> Enum.map(0..8, fn _c -> "" end)
       end
+    )
+  end
+
+  ## Given solver event, produce list of {key, val} that is to be applied to a socket
+  defp process_solver_event(:solution, solution, socket) do
+    solved_puzzle = MinizincResults.get_solution_value(
+      solution,
+      "puzzle"
+    )
+    socket
+    |> update(:sudoku, fn _ -> solved_puzzle end)
+    |> update(:total_solutions, &(&1 + 1))
+    |> update(:stage, fn _ -> @solving end)
+  end
+
+  defp process_solver_event(:summary, summary, socket) do
+    stage = if MinizincResults.get_solution_count(summary) > 0, do: @solved, else: @not_solved
+    socket
+    |> update(:stage, fn _ -> stage end)
+  end
+
+  defp process_solver_event(_event, _data, socket) do
+    socket
+  end
+
+  defp button_name(@start_new) do
+    "Start"
+  end
+
+  defp button_name(@solving) do
+    "Solving..."
+  end
+
+  defp button_name(@solved) do
+    "Solved! Try next one..."
+  end
+
+  defp button_name(@not_solved) do
+    "No solutions. Try next one..."
+  end
+
+  defp action(@start_new) do
+    "solve"
+  end
+
+  defp action(@solving) do
+    "ignore"
+  end
+
+  defp action(@solved) do
+    "next_puzzle"
+  end
+
+  defp action(@not_solved) do
+    "next_puzzle"
+  end
+
+  defp new_puzzle(socket) do
+    assign(
+      socket,
+      [
+        total_solutions: 0,
+        sudoku: empty_sudoku(),
+        stage: @start_new
+      ]
     )
   end
 
