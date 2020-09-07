78b518c0c0ef522528a58f2c355e9d14e7232fc0
Boris Okner <boris.okner@gmail.com>
2020-09-07 11:54:32 -0400

    Add distillery dep

--- mix.exs
+++ mix.exs
@@ -46,6 +46,7 @@ defmodule Solverlview.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
+      {:distillery, "~> 2.0"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
