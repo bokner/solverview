841c81467bcfa21d9f753cd8384d777f9cbeafb6
Boris Okner <boris.okner@gmail.com>
2022-09-20 12:23:15 -0400

    Use Elixir 1.13

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule SolverView.MixProject do
     [
       app: :solverview,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
